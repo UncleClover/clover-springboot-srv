be3a5918ca391b60f71c06f7b318c7ab5851b16f
UncleClover <qiang900714@126.com>
2018-03-26 15:45:05 +0800

    新增jar包配置

--- src/main/test/com/clover/springboot/user/test/UserTest.java
+++ src/main/test/com/clover/springboot/user/test/UserTest.java
@@ -1,11 +1,14 @@
 package com.clover.springboot.user.test;
 
+import java.util.Date;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.clover.api.tools.common.utils.DateUtil;
 import com.clover.springboot.mapper.UserMapper;
 import com.clover.springboot.model.UserInfo;
 
@@ -24,8 +27,8 @@ public class UserTest {
 		user.setPwdRandom("123");
 		user.setName("clover");
 		user.setSex("1");
-		user.setLoginTime("2018-03-21");
-		user.setCreateTime("2018-01-01");
+		user.setLoginTime(DateUtil.random());
+		user.setCreateTime(DateUtil.format(new Date()));
 
 		userMapper.insert(user);
 	}
